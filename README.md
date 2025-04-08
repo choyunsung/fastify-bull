27ae957793f341e02f6d1835897a2c7e94bf367b
yunsung.c <cys@amcg.kr>
2025-04-09 08:44:30 +0900

    ...

--- README.md
+++ README.md
@@ -1,29 +1,30 @@
-# @guivic/fastify-bull
-[![npm version](https://badge.fury.io/js/@guivic%2Ffastify-bull.svg)](https://badge.fury.io/js/@guivic%2Ffastify-bull)
-![nodejs workflow](https://github.com/guivic/fastify-bull/actions/workflows/nodejs.yml/badge.svg)
+# fastify-bull2
+[![npm version](https://badge.fury.io/js/fastify-bull2.svg)](https://badge.fury.io/js/fastify-bull2)
 
 Use bull queues from your fastify application. Utilize fastify decorators to access shared connections, logger etc.
 
-## Installation
+## 설치
 
 ```bash
-npm install @guivic/fastify-bull --save
+npm install fastify-bull2 --save
 
-yarn add @guivic/fastify-bull
+# or
+
+yarn add fastify-bull2
 ```
 
-## Usage
+## 사용법
 
-### Queue Handler
+### 큐 핸들러
 
-An example queue handler.
+큐 핸들러 예제입니다.
 
 ```js
 // queues/example.queue.js
 
 const name = 'example';
-function handler(server, job, done) { // Can also be an async handler
-  /* queue processing logic here */
+function handler(server, job, done) { // 비동기 핸들러도 가능합니다
+  /* 큐 처리 로직 */
   done()
 }
 
@@ -33,23 +34,24 @@ module.exports = {
 };
 ```
 
-### Adding to fastify
-
+### Fastify에 추가하기
 
 ```js
 const fastify = require('fastify');
 
 const server = fastify();
 
-server.register(require('fastify-bull'), {
-  paths: 'queues/**/*.queue.js', // Where to look for queues files
+server.register(require('fastify-bull2'), {
+  paths: 'queues/**/*.queue.js', // 큐 파일을 찾을 경로
+  redisUrl: 'redis://localhost:6379', // Redis 연결 URL
+  // bullOptions: {} // Bull 추가 옵션 (선택 사항)
 })
 
 const start = async () => {
   try {
     await server.listen(3000, "0.0.0.0");
 
-    // add an item to the queue (more information on Bull documentation)
+    // 큐에 작업 추가 (자세한 내용은 Bull 문서를 참조)
     server.queues['example'].add({ hello: 'world' }, { priority: 1 });
 
   } catch (err) {
@@ -62,41 +64,56 @@ const start = async () => {
 start();
 ```
 
-### Adding a task to the queue
+### ESM에서 사용하기
 
-```ts
+```js
+import Fastify from 'fastify';
+import fastifyBull from 'fastify-bull2';
+
+const server = Fastify();
+
+server.register(fastifyBull, {
+  paths: 'queues/**/*.queue.js',
+  redisUrl: 'redis://localhost:6379',
+});
+
+// ... 이하 동일
+```
+
+### 큐에 작업 추가하기
+
+```js
 fastify.queues['my-queue'].add({ data: 'some data' });
 ```
 
-## Options
+## 옵션
 
-* `paths: <String | Array>`  specify folder where queue handlers are present.
-* `redisUrl: <String>` The Redis url connection.
-* `onFailed: <Function>` Queue handler on "failed" event (by default will log with the fastify logger)
-* `onError: <Function>` Queue handler on "error" event (by default will log with the fastify logger)
+* `paths: <String | Array>`: 큐 핸들러가 있는 폴더를 지정합니다.
+* `redisUrl: <String>`: Redis 연결 URL입니다.
+* `redisOptions: <Object>`: Redis 연결 옵션입니다 (선택 사항).
+* `bullOptions: <Object>`: Bull 큐에 전달할 추가 옵션입니다 (선택 사항).
+* `onFailed: <Function>`: 큐 "failed" 이벤트 핸들러입니다 (기본값으로 fastify 로거를 사용).
+* `onError: <Function>`: 큐 "error" 이벤트 핸들러입니다 (기본값으로 fastify 로거를 사용).
 
-## Informations
+## 정보
 
-### Bull and Heroku
+### Bull과 Redis 연결
 
-I use [ioredis](https://github.com/luin/ioredis) for the Redis conneciton. It's preconfigured to share the Redis connection across queues. It's really usefull on environment like Heroku where you have connection limits.
+이 플러그인은 [ioredis](https://github.com/luin/ioredis)를 사용하여 Redis에 연결합니다. 큐 간에 Redis 연결을 공유하도록 사전 구성되어 있어 연결 제한이 있는 환경에서 유용합니다.
 
-## Author
+## 작성자
 
-Ludovic Lérus from [Guivic](https://guivic.io)
-
-Thanks to [Jerry Thomas](https://github.com/jerrythomas) for his fork
+Cho Yunsung
 
 ## TODO
 
-- Add the Typescript typings
-- Add the support of fastify-redis
-- Add the support of Redis connection Object as options
-- Find a way to let people configure the way bull will handle the redis connection
-- Add the support for nested directory
-- Add default parameters for paths
-- Remove the fast-glob dep?
+- TypeScript 타입 정의 추가
+- fastify-redis 지원 추가
+- 옵션으로 Redis 연결 객체 지원 추가
+- Bull이 Redis 연결을 처리하는 방식을 구성할 수 있는 방법 찾기
+- 중첩 디렉토리 지원 추가
+- paths에 대한 기본 매개변수 추가
 
-## License
+## 라이센스
 
-Licensed under [MIT](./LICENSE).
+[MIT](./LICENSE) 라이센스에 따라 사용이 허가됩니다.
--- index.js
+++ index.js
@@ -1,80 +1,1 @@
-const fastifyPlugin = require('fastify-plugin');
-const fastGlob = require('fast-glob');
-const path = require('path');
-const Queue = require('bull');
-const Redis = require('ioredis');
-
-/**
- * Default on failed handler
- * @param {Object} fastify - Fastify instance
- * @param {Object} error - The Bull error
- */
-function defaultOnFailed(fastify, error) {
-	fastify.log.error(`${error.queue.name}`);
-	fastify.log.error(error);
-}
-
-/**
- * Default on error handler
- * @param {Object} fastify - Fastify instance
- * @param {Object} error - The Bull error
- */
-function defaultOnError(fastify, error) {
-	fastify.log.error(`${error.queue.name}`);
-	fastify.log.error(error);
-}
-
-/**
- * Load every queue file and decorate the Fastify instance.
- * @param {Object} fastify - Fastify instance
- * @param {Object} options - Plugin's options
- * @param {Function} next - Fastify next callback
- */
-function fastifyBull(fastify, options, next) {
-	const files = fastGlob.sync(options.paths);
-	const queues = {};
-
-	const client = new Redis(options.redisUrl);
-	const subscriber = new Redis(options.redisUrl);
-
-	const queueOptions = {
-		createClient(type) {
-			switch (type) {
-			case 'client': return client;
-			case 'subscriber': return subscriber;
-			default: return new Redis(options.redisUrl);
-			}
-		},
-	};
-
-	if (!options.onFailed) {
-		options.onFailed = defaultOnFailed;
-	} if (!options.onError) {
-		options.onError = defaultOnError;
-	}
-
-	for (let i = 0; i < files.length; i++) {
-		const queueConfig = require(path.resolve(files[i]));
-		const filename = path.basename(files[i]);
-		const queueName = filename.split('.')[0];
-
-		if (queueName && queueConfig.name && queueConfig.handler) {
-			queues[queueName] = new Queue(
-				queueConfig.name,
-				queueOptions,
-			);
-
-			queues[queueName].process((job) => queueConfig.handler(fastify, job));
-
-			queues[queueName].on('error', (error) => options.onError(fastify, error));
-			queues[queueName].on('failed', (error) => options.onFailed(fastify, error));
-		}
-	}
-
-	fastify.decorate('queues', queues);
-	next();
-}
-
-module.exports = fastifyPlugin(fastifyBull, {
-	name: 'fastify-bull',
-});
+// CommonJSconst fastifyPlugin = require('fastify-plugin');const fastGlob = require('fast-glob');const path = require('path');const Queue = require('bull');const Redis = require('ioredis');/** * Default on failed handler * @param {Object} fastify - Fastify instance * @param {Object} error - The Bull error */function defaultOnFailed(fastify, error) {	fastify.log.error(`${error.queue.name}`);	fastify.log.error(error);}/** * Default on error handler * @param {Object} fastify - Fastify instance * @param {Object} error - The Bull error */function defaultOnError(fastify, error) {	fastify.log.error(`${error.queue.name}`);	fastify.log.error(error);}/** * Load every queue file and decorate the Fastify instance. * @param {Object} fastify - Fastify instance * @param {Object} options - Plugin's options * @param {Function} next - Fastify next callback */function fastifyBull(fastify, options, next) {	const files = fastGlob.sync(options.paths);	const queues = {};	// redis 옵션 기본값 설정	const redisOptions = options.redisOptions || {};	// bull 옵션 기본값 설정	const bullOptions = options.bullOptions || {};	const client = new Redis(options.redisUrl, redisOptions);	const subscriber = new Redis(options.redisUrl, redisOptions);	const queueOptions = {		createClient(type) {			switch (type) {			case 'client': return client;			case 'subscriber': return subscriber;			default: return new Redis(options.redisUrl, redisOptions);			}		},		...bullOptions, // bull 추가 옵션 적용	};	if (!options.onFailed) {		options.onFailed = defaultOnFailed;	}	if (!options.onError) {		options.onError = defaultOnError;	}	for (let i = 0; i < files.length; i++) {		const queueConfig = require(path.resolve(files[i]));		const filename = path.basename(files[i]);		const queueName = filename.split('.')[0];		if (queueName && queueConfig.name && queueConfig.handler) {			queues[queueName] = new Queue(				queueConfig.name,				queueOptions,			);			queues[queueName].process((job) => queueConfig.handler(fastify, job));			queues[queueName].on('error', (error) => options.onError(fastify, error));			queues[queueName].on('failed', (error) => options.onFailed(fastify, error));		}	}	fastify.decorate('queues', queues);	next();}// CommonJS 내보내기const plugin = fastifyPlugin(fastifyBull, {	name: 'fastify-bull',});module.exports = plugin;// ESM 호환성을 위한 기본 내보내기 추가module.exports.default = plugin;
